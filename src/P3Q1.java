f9280ef3e6ddfb068e87f56fda9f260f4413cd34
janarth10 <janarthp10@gmail.com>
2017-11-14 19:06:11 -0500

    implemented sort

--- src/P3Q1.java
+++ src/P3Q1.java
@@ -1,19 +1,33 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 
 public class P3Q1 {
 
     public static void main(String[] args) {
-        class Edge{
+        class Edge implements Comparable<Edge>{
             int start, end, weight;
             public Edge(String[] edge_input) {
                 start = Integer.parseInt(edge_input[0]);
                 end = Integer.parseInt(edge_input[1]);
                 weight = Integer.parseInt(edge_input[2]);
             }
+
+            @Override
+            public int compareTo(Edge e){
+                if (weight < e.weight){
+                    return -1;
+                }
+                return 1;
+            }
+
+            public String toString(){
+                return String.format("start: %d, End: %d, Weight: %d", start, end, weight);
+            }
+
         }
 
         Scanner scan = new Scanner(System.in);
