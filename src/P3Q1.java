1d9c2bc070190af1eccc8436e27a73d14b9a8b9c
janarth10 <janarthp10@gmail.com>
2017-11-18 22:41:04 -0500

    passed basic tests

--- src/P3Q1.java
+++ src/P3Q1.java
@@ -1,6 +1,3 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.ArrayList;
 import java.util.*;
 import java.util.Scanner;
 import java.util.Vector;
@@ -12,49 +9,58 @@ public class P3Q1 {
         Scanner scan = new Scanner(System.in);
         int n = Integer.parseInt(scan.nextLine());
         int m = Integer.parseInt(scan.nextLine());
-        float t = Float.parseFloat(scan.nextLine());
+        double t = Double.parseDouble(scan.nextLine());
 
-        class Edge implements Comparable<Edge>{
+        class Edge {
             int start, end, weight;
             public Edge(){}
 
             public Edge(String[] edge_input) {
-                int a = Integer.parseInt(edge_input[1]);
                 start = Integer.parseInt(edge_input[0]);
-                end = a;
-                if (a < start){
+                end = Integer.parseInt(edge_input[1]);
+                if (end < start){
+                    int a = end;
                     end = start;
                     start = a;
                 }
                 weight = Integer.parseInt(edge_input[2]);
             }
 
-            @Override
-            public int compareTo(Edge e){
-                if (weight < e.weight){
-                    return -1;
-                }
-                return 1;
-            }
-
             public String toString(){
                 return String.format("start: %d, End: %d, Weight: %d", start, end, weight);
             }
 
-            // Calculate shortest path using Bellman Ford's algo
-            public int shortest_path(Vector<Edge> H, int source, int v){
+            public String edgePointsToString(){
+                return String.format("%d %d", start, end);
+            }
+
+            // Calculate shortest path using Bellman Ford's algo given edges in H
+            public int shortest_path(Vector<Edge> H, int source, int end){
                 int[] distances = new int[n+1];
                 Arrays.fill(distances, Integer.MAX_VALUE);
                 distances[source] = 0;
 
+                int u, v = 0;
                 for(int i = 0; i < n; i++){
                     for(Edge e: H){
-                        distances[e.end] = Math.min(distances[e.end],
-                                            distances[e.start] + e.weight);
+                        u = e.start;
+                        v = e.end;
+                        if(v != source) {
+                            distances[v] = Math.min(distances[v],
+                                    Math.max(distances[u], distances[u] + e.weight));
+                        }
+                        if(u != source){
+                            distances[u] = Math.min(distances[u],
+                                    Math.max(distances[v], distances[v] + e.weight));
+                        }
                     }
                 }
 
-                return distances[v];
+//                System.out.println(H);
+//                System.out.printf("%d %d", source, end);
+//                System.out.println("");
+//                System.out.println(Arrays.toString(distances));
+                return distances[end];
             }
         }
 
@@ -63,17 +69,41 @@ public class P3Q1 {
             edges[i] = new Edge(scan.nextLine().split(" "));
         }
 
-        //start of algo A
-        Arrays.sort(edges);
-        Vector<Edge> H = new Vector<Edge>();
-        Edge dummy = new Edge();
+        // start of algo A
+        Arrays.sort(edges, new Comparator<Edge>() {
+            @Override
+            public int compare(Edge e1, Edge e2) {
+                return e1.weight - e2.weight;
+            }
+        });
+        Vector<Edge> H = new Vector<>();
+        int weightOfH = 0;
+        int d = 0;
+        Edge dummy = new Edge(); // used solely to call shortest_path()
         for(int i = 0; i < m; i++){
-            int d = dummy.shortest_path(H, edges[i].start, edges[i].end);
+            d = dummy.shortest_path(H, edges[i].start, edges[i].end);
             if(t * edges[i].weight < d){
                 H.add(edges[i]);
+                weightOfH += edges[i].weight;
             }
         }
 
-        System.out.println(H);
+        // sorting according to output req specified in assignment
+        H.sort(new Comparator<Edge>() {
+            @Override
+            public int compare(Edge e1, Edge e2) {
+                if(e1.start == e2.start){
+                    return e1.end - e2.end;
+                }
+                return e1.start - e2.start;
+            }
+        });
+
+        // solution
+        System.out.println(H.size());
+        System.out.println(weightOfH);
+        for(Edge e: H){
+            System.out.println(e.edgePointsToString());
+        }
     }
 }
---
+++ src/test.java
@@ -0,0 +1,9 @@
+///**
+// * Created by janar on 2017-11-18.
+// */
+public class test {
+    public static void main(String[] args) {
+        System.out.printf("%d", Integer.MAX_VALUE + 3);
+
+    }
+}
